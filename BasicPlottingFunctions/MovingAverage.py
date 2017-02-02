089333f237fb1dbd06ca62e398c039b9b86f99ff
Eddie Ball (mac) <eball@vt.edu>
2017-02-01 20:57:40 -1000

    first stab at Moveing_Average

--- BasicPlottingFunctions/MovingAverage.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 31 16:18:57 2017
-
-@author: eddieball
-"""
-
---
+++ BasicPlottingFunctions/Moving_Average.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 16:18:57 2017
+
+@author: eddieball
+"""
+
+def MovingAverage(n,X):
+    num_stocks = len(X) #number of stocks minus 1
+    MA = [[] for x in range(num_stocks)]  # moving average list
+    # Pad MA with n zeros
+    for i in range(1,num_stocks):
+        for j in range(0,n):
+            X[i].insert(0,0)
+      
+    for i in range(1,num_stocks):
+        for j in range(n,len(X[i])):
+            n_sum = 0
+            for k in range(0,n):
+                n_sum = n_sum + X[i][j-k]
+            MA[i].append(n_sum/n) 
+    return MA
