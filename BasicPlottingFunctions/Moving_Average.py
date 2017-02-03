ea8338b77ef338f60e7b46c24528bd65c81ceaf8
Eddie Ball (mac) <eball@vt.edu>
2017-02-02 16:03:23 -1000

    moving average gives moving average of a list of list of numbers

--- BasicPlottingFunctions/Moving_Average.py
+++ BasicPlottingFunctions/Moving_Average.py
@@ -7,14 +7,17 @@ Created on Tue Jan 31 16:18:57 2017
 """
 
 def MovingAverage(n,X):
+    # n = number of days to average over
+    # X = Data to take moving average of
+    
     num_stocks = len(X) #number of stocks minus 1
     MA = [[] for x in range(num_stocks)]  # moving average list
     # Pad MA with n zeros
     for i in range(1,num_stocks):
         for j in range(0,n):
             X[i].insert(0,0)
-      
-    for i in range(1,num_stocks):
+          
+    for i in range(0,num_stocks):
         for j in range(n,len(X[i])):
             n_sum = 0
             for k in range(0,n):
---
+++ BasicPlottingFunctions/Test_Script.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 11:30:39 2017
+
+@author: eddieball
+"""
+import matplotlib.pyplot as plt
+import numpy as np # loadtxt
+import pandas as pd
+from Moving_Average import MovingAverage
+
+n       = 5
+data    = pd.read_csv("shareprices.csv",header='infer')
+X       = np.asarray(data.values).T.tolist()[1:-1]
+
+MA = MovingAverage(n,X)
+
+plt.figure(1)
+for i in range(0,10):
+    plt.plot(MA[i][n-1:])
