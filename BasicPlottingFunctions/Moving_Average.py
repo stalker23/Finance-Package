3f9342f6049b8d2c7548abc34f186b9fb665cfad
Eddie Ball (mac) <eball@vt.edu>
2017-02-03 10:02:42 -1000

    Corrected order and modified axis titles

--- BasicPlottingFunctions/Moving_Average.py
+++ BasicPlottingFunctions/Moving_Average.py
@@ -12,8 +12,9 @@ def MovingAverage(n,X):
     
     num_stocks = len(X) #number of stocks minus 1
     MA = [[] for x in range(num_stocks)]  # moving average list
+    
     # Pad MA with n zeros
-    for i in range(1,num_stocks):
+    for i in range(0,num_stocks):
         for j in range(0,n):
             X[i].insert(0,0)
           
@@ -22,5 +23,5 @@ def MovingAverage(n,X):
             n_sum = 0
             for k in range(0,n):
                 n_sum = n_sum + X[i][j-k]
-            MA[i].append(n_sum/n) 
+            MA[i].append(n_sum/n)
     return MA
--- BasicPlottingFunctions/Test_Script.py
+++ BasicPlottingFunctions/Test_Script.py
@@ -6,18 +6,27 @@ Created on Thu Feb  2 11:30:39 2017
 @author: eddieball
 """
 import matplotlib.pyplot as plt
+import matplotlib.dates as dates
 import numpy as np # loadtxt
 import pandas as pd
 from Moving_Average import MovingAverage
 
-n       = 10
-data    = pd.read_csv("shareprices.csv",header='infer')
-X       = np.asarray(data.values).T.tolist()[1:-1]
+n       = 1
+data    = pd.read_csv("shareprices.csv",header='infer',skiprows=1)
+X       = np.asarray(data.values).T.tolist()[0:-1] # Transpose dataframe and make list
+# sort date from past to present
+for i in range(0,len(X)):
+    X[i].reverse()
 
-MA = MovingAverage(n,X)
+MA = MovingAverage(n,X[1:])
 
-plt.figure(1)
-for i in range(0,10):
-    plt.plot(MA[i][n-1:],label=list(data)[i+1])
+fig1 = plt.figure(1)
+for i in range(0,len(MA)):
+    plt.plot_date(dates.datestr2num(X[0]),MA[i][n-1:],'-',label=list(data)[i+1])
+
+plt.xlabel('Date')
+plt.ylabel('Price [$]')
+plt.title('TSP Funds: ' + str(n) + ' Day Moving Average' )
+fig1.autofmt_xdate()
 plt.legend()
 plt.show()
