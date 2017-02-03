17db39c632e2cd81843282067b2b2e30361136b5
Eddie Ball (mac) <eball@vt.edu>
2017-02-02 16:56:06 -1000

    added legends

--- BasicPlottingFunctions/Test_Script.py
+++ BasicPlottingFunctions/Test_Script.py
@@ -10,7 +10,7 @@ import numpy as np # loadtxt
 import pandas as pd
 from Moving_Average import MovingAverage
 
-n       = 5
+n       = 10
 data    = pd.read_csv("shareprices.csv",header='infer')
 X       = np.asarray(data.values).T.tolist()[1:-1]
 
@@ -18,4 +18,6 @@ MA = MovingAverage(n,X)
 
 plt.figure(1)
 for i in range(0,10):
-    plt.plot(MA[i][n-1:])
+    plt.plot(MA[i][n-1:],label=list(data)[i+1])
+plt.legend()
+plt.show()
