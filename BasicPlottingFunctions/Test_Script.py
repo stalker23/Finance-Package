c13d4f30da77eb3fc3cec0b5d8e3813e03e6aa71
Eddie Ball (mac) <eball@vt.edu>
2017-06-02 21:51:59 -0400

    fixed plotting for n>1

--- BasicPlottingFunctions/Test_Script.py
+++ BasicPlottingFunctions/Test_Script.py
@@ -11,7 +11,7 @@ import numpy as np # loadtxt
 import pandas as pd
 from Moving_Average import MovingAverage
 
-n       = 1
+n       = 10
 data    = pd.read_csv("shareprices.csv",header='infer',skiprows=1)
 X       = np.asarray(data.values).T.tolist()[0:-1] # Transpose dataframe and make list
 # sort date from past to present
@@ -19,10 +19,12 @@ for i in range(0,len(X)):
     X[i].reverse()
 
 MA = MovingAverage(n,X[1:])
+for i in range(0,len(MA)):
+    print(len(MA[i]))
 
 fig1 = plt.figure(1)
 for i in range(0,len(MA)):
-    plt.plot_date(dates.datestr2num(X[0]),MA[i][n-1:],'-',label=list(data)[i+1])
+    plt.plot_date(dates.datestr2num(X[0][n-1:]),MA[i][n-1:],'-',label=list(data)[i+1])
 
 plt.xlabel('Date')
 plt.ylabel('Price [$]')
